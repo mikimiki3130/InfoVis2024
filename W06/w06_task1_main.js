4fedc4f20a2a6dab51bebe4bf9d22d768a6d4b95
みきみき <kmikimiki5454@gmail.com>
2024-11-14 12:26:49 +0900

    Update task1

--- W06/w06_task1_main.js
+++ W06/w06_task1_main.js
@@ -66,12 +66,12 @@ class ScatterPlot {
         let self = this;
         const x_margin =10, y_margin =10;
 
-        const xmin = d3.min( self.data, d => d.x -x_margin);
-        const xmax = d3.max( self.data, d => d.x +x_margin);
+        const xmin = d3.min(self.data, d => d.x) -x_margin;
+        const xmax = d3.max( self.data, d => d.x) +x_margin;
         self.xscale.domain( [xmin, xmax] ); // 倍数の場合、minが小さすぎるとうまくいかないため定数を使用
 
-        const ymin = d3.min( self.data, d => d.y -y_margin);
-        const ymax = d3.max( self.data, d => d.y +y_margin);
+        const ymin = d3.min( self.data, d => d.y)  -y_margin;
+        const ymax = d3.max( self.data, d => d.y)  +y_margin;
         self.yscale.domain( [ymin, ymax] );
 
         self.render();
